1f88874eba9fdf561f92786ce1f22fb27a7e756f
Aliza Buchen <alizabuchen@alizas-air-2.mynetworksettings.com>
2024-10-09 13:49:25 -0400

    tests

--- src/test/java/buchen/gameoflife/GameOfLifeTest.java
+++ src/test/java/buchen/gameoflife/GameOfLifeTest.java
@@ -1,10 +1,24 @@
 package buchen.gameoflife;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class GameOfLifeTest {
+    private Clipboard clipboard;
+
+    @BeforeEach
+    public void setup() {
+        clipboard = Mockito.mock(Clipboard.class);
+    }
 
     @Test
     public void nextGen()
@@ -20,20 +34,25 @@ public class GameOfLifeTest {
         assertEquals("00000\n00000\n00000\n11100\n00000\n", board.toString());
     }
 
-//    @Test
-//    public void loadFromRle() {
-//        //given
-//        GameOfLife game = new GameOfLife(5, 5);
-//        String rleContent = """
-//            #C This is a glider.
-//            x = 3, y = 3
-//            bo$2bo$3o!
-//        """;
-//
-//        //when
-//        game.loadFromRle(rleContent);
-//
-//        //then
-//        assertEquals("010\n001\n111\n", game.toString());
-//    }
+    @Test
+    public void clipboardRleString() throws IOException, UnsupportedFlavorException {
+        //given
+        GameOfLife board = new GameOfLife(100,100);
+        String rleData = """
+            #C This is a glider.
+            x = 3, y = 3
+            bo$2bo$3o!
+        """;
+
+        //when
+        when(clipboard.getData(DataFlavor.stringFlavor)).thenReturn(rleData);
+        board.loadFromRle(rleData);
+
+        //then
+        assertEquals(true, board.isAlive(1,0));
+        assertEquals(true, board.isAlive(2,1));
+        assertEquals(true, board.isAlive(2,2));
+        assertEquals(true, board.isAlive(1,2));
+        assertEquals(true, board.isAlive(0,2));
+    }
 }
